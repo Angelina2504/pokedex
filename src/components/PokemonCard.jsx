7bdcfd2b500cdbf6f469b51086b59a8bb7b07094
Angélina Droz <angieso91@gmail.com>
2024-03-27 11:19:50 +0100

    quête 3 finito

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,11 +1,31 @@
 function PokemonCard () {
+    let pokemon = pokemonList[1];
     return (
-        <>
-        <figure><img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbisaur" /></figure>
-
-        <figcaption>Bulbasaur</figcaption>
-        </>
+    <figure>
+      {pokemon.imgSrc === undefined ? (
+        <div><figcaption>{pokemon.name}</figcaption>
+        <p>???</p>
+        </div>
+      ) : ( 
+        <div>
+          <figcaption>{pokemon.name}</figcaption>
+          <img src={pokemon.imgSrc}/>
+        </div>
+      )}
+    </figure>
     );
 }
 
 export default PokemonCard;
+
+const pokemonList = [
+    {
+      name: "bulbasaur",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+  
