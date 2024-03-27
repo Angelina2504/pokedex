c9a53f7467308fb2477e4d703306eb88babfa53d
Angélina Droz <angieso91@gmail.com>
2024-03-27 11:54:45 +0100

    quête 4

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -1,5 +1,4 @@
-function PokemonCard () {
-    let pokemon = pokemonList[1];
+function PokemonCard ({ pokemon }) {
     return (
     <figure>
       {pokemon.imgSrc === undefined ? (
@@ -14,18 +13,10 @@ function PokemonCard () {
       )}
     </figure>
     );
+    
 }
 
 export default PokemonCard;
 
-const pokemonList = [
-    {
-      name: "bulbasaur",
-      imgSrc:
-        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-    },
-    {
-      name: "mew",
-    },
-  ];
+
   
