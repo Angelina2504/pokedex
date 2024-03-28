db92879d0a5df8f22e195c8eca78dad3cdb97e40
Angélina Droz <angieso91@gmail.com>
2024-03-28 17:00:18 +0100

    state 2

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,14 @@
+export default function NavBar ({pokemonList, pokemonIndex, handleClickSuivant, handleclickPrécédent}) {
+    
+
+    return (
+    <>
+            {
+         pokemonIndex > 0 ? <button onClick={handleclickPrécédent}>Précédent</button> : undefined
+         }  
+
+        {
+          pokemonIndex <pokemonList.length -1 ? <button onClick={handleClickSuivant}>Suivant</button> : undefined } 
+    </>  
+    );
+}
